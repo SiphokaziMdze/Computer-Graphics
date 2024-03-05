d1d2bc6cefde9f8a778300257143cffc58c502af
SIPHOKAZI VERONICA MDZE <veronicamdze@gmail.com>
2024-03-05 22:39:42 +0000

    updated main.js file

---
+++ A cube/main2.js
@@ -0,0 +1,251 @@
+const canvas  = document.querySelector('canvas');
+const gl = canvas.getContext('webgl');
+
+if(!gl){
+throw new Error("webGL not supported");
+}
+gl.clearColor(1,0,1,1);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+const vertices = new Float32Array(
+[
+    0.5, 0.5, -0.5,
+    0.5, -0.5, -0.5,
+    -0.5, -0.5, -0.5,
+     0.5, 0.5, -0.5,
+    -0.5, -0.5, -0.5,
+    -0.5, 0.5, -0.5,
+
+    -0.5, 0.5, -0.5,
+      -0.5, -0.5, -0.5,
+       -0.5, -0.5, 0.5,
+       -0.5, 0.5, -0.5,
+       -0.5, -0.5, 0.5,
+       -0.5, 0.5, 0.5,
+
+     -0.5,0.5,0.5,
+     -0.5,-0.5,0.5,
+     0.5,0.5,0.5,
+     -0.5,-0.5,0.5,
+     0.5,0.5,0.5,
+     0.5,-0.5,0.5,
+
+    0.5,0.5,-0.5,
+    0.5,-0.5,-0.5,
+     0.5,-0.5,0.5,
+     0.5,0.5,-0.5,
+     0.5,-0.5,0.5,
+     0.5,0.5,0.5,
+
+     0.5,0.5,-0.5,
+     0.5,0.5,0.5,
+     -0.5,0.5,0.5,
+     0.5,0.5,-0.5,
+     -0.5,0.5,-0.5,
+     -0.5,0.5,0.5,
+
+      0.5,-0.5,-0.5,
+      0.5,-0.5,0.5,
+      -0.5,-0.5,0.5,
+      0.5,-0.5,-0.5,
+      -0.5,-0.5,-0.5,
+      -0.5,-0.5,0.5,
+
+
+]); //My coordinates....
+
+const colors = new Float32Array
+([
+
+     1,0,0,
+     1,0,0, 
+     1,0,0,
+
+     1,0,0,
+     1,0,0,
+     1,0,0,
+
+
+0,1,0,
+0,1,0,
+0,1,0,
+
+0,1,0,
+0,1,0,
+0,1,0,
+
+
+0,0,1,
+0,0,1,
+0,0,1,
+
+0,0,1,
+0,0,1,
+0,0,1,
+
+1,1,0,
+1,1,0,
+1,1,0,
+
+1,1,0,
+1,1,0,
+1,1,0,
+
+1,1,1,
+1,1,1,
+1,1,1,
+
+1,1,1,
+1,1,1,
+1,1,1,
+
+0,1,1,
+0,1,1,
+0,1,1,
+
+0,1,1,
+0,1,1,
+0,1,1,
+
+
+
+]);// color data
+
+const buff = gl.createBuffer(); // create buffer for x and y coordinates
+const colorbuff = gl.createBuffer(); // create buffer for color
+
+gl.bindBuffer(gl.ARRAY_BUFFER, buff);
+gl.bufferData(gl.ARRAY_BUFFER,vertices, gl.STATIC_DRAW);
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertexShader, `
+attribute vec3 pos;//pos.x and pos.y pos.z
+attribute vec3 color;
+varying vec3 fragcolor;
+
+uniform float shift;
+//float x,y;
+float c = cos(shift);
+float s = sin(shift);
+void main(){
+
+    fragcolor = color;
+    //..............z axis rotation..................
+    //gl_Position.x = pos.x*c-pos.y*s;
+    //gl_Position.y = pos.y*c+pos.x*s;
+
+    //gl_Position.z = 0.0;
+    //gl_Position.w = 1.0;
+    //................. x axis rotation...............
+
+    gl_Position.y = pos.y*c - pos.z*s;
+    gl_Position.z = pos.z*c + pos.y*s;
+
+    gl_Position.x = pos.x;
+    gl_Position.w = 1.0;
+
+  /* 
+gl_Position.x = pos.x*c + pos.z*s;
+gl_Position.z = pos.z*c - pos.x*s;
+
+gl_Position.y = pos.y;
+gl_Position.w = 1.0;
+*/
+
+//gl.Position.z = pos.y*cos(0.9) - pos.z*sin(0.9);
+//gl.Position.y = pos.y*sin(0.9) + pos.z*cos(0.9);
+
+
+/*
+    x = pos.x*cos(shift) - pos.y*sin(shift);
+    y = pos.y*cos(shift) - pos.x*sine(shift); 
+
+
+fragcolor = color;
+// gl_Position = vec4(x,y,0,1);
+//gl_Position = vec4(pos,0,1);
+//gl_Position = vec4(pos,0,1) + vec4(shift, 0, 0, 1);
+//gl_Position = vec4(pos*0.5,0,1)+ vec4(1, 0, 0, 1);
+*/     
+}
+`  );
+gl.compileShader(vertexShader);
+
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragmentShader, `
+
+precision mediump float;
+varying vec3 fragcolor;
+
+void main(){
+gl_FragColor = vec4(fragcolor,1); //Color of the inside shape...
+//gl_FragColor = vec4(0,0,1,1); //Color of the inside shape...
+}
+ `);
+gl.compileShader(fragmentShader);
+
+const program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+gl.useProgram(program);
+
+gl.enable(gl.DEPTH_TEST);
+
+const positionLocation = gl.getAttribLocation(program, 'pos');
+const uniformLocation = gl.getUniformLocation(program,`shift`);
+
+gl.enableVertexAttribArray(positionLocation);
+gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0,);
+
+
+//gl.drawArrays(gl.TRIANGLES, 0, 24);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, colorbuff);
+gl.bufferData(gl.ARRAY_BUFFER, colors,gl.STATIC_DRAW);
+
+const colorLocation = gl.getAttribLocation(program, 'color');
+gl.enableVertexAttribArray(colorLocation);
+gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0,);
+
+
+var myshift = 0;
+var shift = 0.01;
+
+draw();
+
+function draw()
+{
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+myshift += shift;
+
+gl.uniform1f(uniformLocation,myshift);
+gl.drawArrays(gl.TRIANGLES, 0, 90);
+window.requestAnimationFrame(draw);
+}
+
+function stop()
+{
+
+shift = shift*0 ;
+
+}
+function start()
+{
+ shift = 0.01;
+}
+
+function speedup()
+{
+
+shift = shift*2;
+
+}
+
+function speedlow()
+{
+
+shift = shift/2;
+
+}
