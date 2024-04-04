5b293f8bf9f3c6f098a7a2aa6febd2b328332b7c
SIPHOKAZI VERONICA MDZE <veronicamdze@gmail.com>
2024-04-04 01:49:51 +0000

    updated main3.js
    I could not draw the house I wanted, but this open envelope looks cute.

--- Line/main3.js
+++ Line/main3.js
@@ -4,7 +4,7 @@ const gl = canvas.getContext(`webgl`);
 gl.clearColor(0.5, 0.6, 0.5, 0);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
-const vertices = new Float32Array([]);
+const vertices = new Float32Array([-0.9,0.1, 0.9,0.1, 0.9,-0.9, -0.9,-0.9, -0.9,0.1, 0,1, 0.9,0.1]);
 
 const buffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
@@ -17,6 +17,20 @@ const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, vsSource);
 gl.compileShader(vertexShader);
 
-const fsSource = `void main() {gl_FragColor = vec4(0.5, 1.0, 0.5, 1); }`;
+const fsSource = `void main() {gl_FragColor = vec4(1.0, 1.0, 1.0, 1); }`;
 
-const fragmentShader = 
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragmentShader, fsSource);
+gl.compileShader(fragmentShader);
+
+const program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+gl.useProgram(program);
+
+const position = gl.getAttribLocation(program, `pos`);
+gl.enableVertexAttribArray(position);
+gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0);
+
+gl.drawArrays(gl.LINE_LOOP, 0, 9);
