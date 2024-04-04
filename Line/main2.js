5e8b38e4f69f3f5d2fb8067e9fab987fea14c6db
SIPHOKAZI VERONICA MDZE <veronicamdze@gmail.com>
2024-04-04 00:29:22 +0000

    created main2,js another js file
    In the file I used lines to draw a kite.

---
+++ Line/main2.js
@@ -0,0 +1,36 @@
+const canvas = document.querySelector(`canvas`);
+const gl = canvas.getContext(`webgl`);
+
+gl.clearColor(0, 1.0, 1.0, 0.3);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+const vertices = new Float32Array([-0.5,0, 0.5,0, 0,-1, -0.5,0, 0,0.5, 0,-1, 0.5,0, 0,0.5]);
+
+const buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+const vsSource = `attribute vec2 pos;
+                  void main() { gl_Position = vec4(pos, 0, 1);}`;
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertexShader, vsSource);
+gl.compileShader(vertexShader);
+
+const fsSource = `void main() { gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0); }`;
+
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragmentShader, fsSource);
+gl.compileShader(fragmentShader);
+
+const program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+gl.useProgram(program);
+
+const position = gl.getAttribLocation(program, `pos`);
+gl.enableVertexAttribArray(position);
+gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0);
+
+gl.drawArrays(gl.LINE_LOOP, 0, 8);
